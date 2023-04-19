c75e2b1528d653370c4891d6aa725d6b210bab17
Anton <s224268@student.dtu.dk>
2023-04-19 05:28:36 +0200

    IT WORKS IT FUCKING WORKS I JUST HAD SEVEN ORGASMS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     static int numberOfIslands;
-    static boolean[] visited;
     static int numberOfFerries;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String lineOne = reader.readLine();
+        String[] partsOne = lineOne.split(" ");
+        numberOfIslands = Integer.parseInt(partsOne[0]);
+        int maxBridges = Integer.parseInt(partsOne[1]);
+        numberOfFerries = Integer.parseInt(partsOne[2]);
+        /*
         numberOfIslands = scanner.nextInt();
         int maxBridges = scanner.nextInt();
         numberOfFerries = scanner.nextInt();
-        //Edge[] edges = new Edge[maxBridges + numberOfFerries - 1];
+
+         */
         ArrayList<Edge>[] islands = new ArrayList[numberOfIslands + 1];
 
         for(int i = 0; i <= numberOfIslands;i++){
@@ -18,52 +28,45 @@ public class Main {
         ArrayList<Edge> edges = new ArrayList<> ();
 
         for (int i = 0; i < maxBridges; i++){
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            int c = scanner.nextInt();
+            String line = reader.readLine();
+            String[] parts = line.split(" ");
+            int a = Integer.parseInt(parts[0]);
+            int b = Integer.parseInt(parts[1]);
+            int c = Integer.parseInt(parts[2]);
             Edge e = new Edge(a,b,c);
-            edges.add(e); //Makes the edge and adds it to the array
+            edges.add(e);
             islands[a].add(e);
-            islands[b].add(e);//adds the newly made edge to its' owner island
+            islands[b].add(e);
         }
         Collections.sort(edges);
-        ArrayList<Integer> prices = new ArrayList<>();
-        ArrayList<Edge> MSTedges = new ArrayList<>();
+        int[] prizes = new int[numberOfIslands - 1];
 
 
         BitSet visitedIslands = new BitSet(numberOfIslands + 1);
         int e = 0;
-        int j = 0;
-        //ArrayList<Edge> possibleEdges = new ArrayList<>();
         visitedIslands.set(1);
         PriorityQueue<Edge> queue = new PriorityQueue<>();
         queue.addAll(islands[1]);
-        //possibleEdges.addAll(islands[1]);
 
         while (e < numberOfIslands - 1){
-            Edge cheapestEdge = null;
-            cheapestEdge = queue.poll();
-            MSTedges.add(cheapestEdge);
+            Edge cheapestEdge = queue.poll();
             if (!visitedIslands.get(cheapestEdge.src)){ //If cheapest route endpoint hasnt been visited //TODO: Add thingy in case it has been visited
                 queue.addAll(islands[cheapestEdge.src]); //Adds all routes from the destination of the cheapest edge
                 visitedIslands.set(cheapestEdge.src); //Sets cheapest route as visited
-                prices.add(cheapestEdge.weight); //Adds the price, for keeping tally
+                prizes[e] = cheapestEdge.weight;
                 e++;
             }
             if (!visitedIslands.get(cheapestEdge.dest)){ //If cheapest route endpoint hasnt been visited //TODO: Add thingy in case it has been visited
                 queue.addAll(islands[cheapestEdge.dest]); //Adds all routes from the destination of the cheapest edge
                 visitedIslands.set(cheapestEdge.dest); //Sets cheapest route as visited
-                prices.add(cheapestEdge.weight); //Adds the price, for keeping tally
+                prizes[e] = cheapestEdge.weight;
                 e++;
             }
-            if (j < numberOfIslands){
-                j++;
-            }
         }
-        Collections.sort(prices);
+        Arrays.parallelSort(prizes);
         int totalPris = 0;
         for(int i = 0; i < numberOfIslands-numberOfFerries-1;i++){
-            totalPris += prices.get(i);
+            totalPris += prizes[i];
         }
         System.out.println(totalPris);
     }
@@ -80,7 +83,6 @@ class Edge implements Comparable<Edge> {
     }
     @Override
     public int compareTo(Edge o) {
-        if (this == null) return Integer.MAX_VALUE;
         if (o == null) return Integer.MIN_VALUE;
         return this.weight - o.weight;
     }
