2a43ec2103e97cbfac5204032c2c72a6c157a6c7
Anton <s224268@student.dtu.dk>
2023-04-19 04:52:19 +0200

    Made some queues, made some shit work. Just needs to be faster

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,21 +9,24 @@ public class Main {
         numberOfIslands = scanner.nextInt();
         int maxBridges = scanner.nextInt();
         numberOfFerries = scanner.nextInt();
-        Edge[] edges = new Edge[maxBridges + numberOfFerries - 1];
+        //Edge[] edges = new Edge[maxBridges + numberOfFerries - 1];
         ArrayList<Edge>[] islands = new ArrayList[numberOfIslands + 1];
 
         for(int i = 0; i <= numberOfIslands;i++){
             islands[i] = new ArrayList<>();
         }
+        ArrayList<Edge> edges = new ArrayList<> ();
 
         for (int i = 0; i < maxBridges; i++){
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             int c = scanner.nextInt();
-            edges[i] = new Edge(a,b,c); //Makes the edge and adds it to the array
-            islands[a].add(edges[i]); //adds the newly made edge to its' owner island
+            Edge e = new Edge(a,b,c);
+            edges.add(e); //Makes the edge and adds it to the array
+            islands[a].add(e);
+            islands[b].add(e);//adds the newly made edge to its' owner island
         }
-        Arrays.parallelSort(edges);
+        Collections.sort(edges);
         ArrayList<Integer> prices = new ArrayList<>();
         ArrayList<Edge> MSTedges = new ArrayList<>();
 
@@ -31,94 +34,39 @@ public class Main {
         BitSet visitedIslands = new BitSet(numberOfIslands + 1);
         int e = 0;
         int j = 0;
-        ArrayList<Edge> possibleedges = new ArrayList<>();
-        possibleedges.addAll(islands[1]);//Adds all edges from the 0´th island, making it our starting point
+        //ArrayList<Edge> possibleEdges = new ArrayList<>();
         visitedIslands.set(1);
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        queue.addAll(islands[1]);
+        //possibleEdges.addAll(islands[1]);
 
         while (e < numberOfIslands - 1){
-            if (visitedIslands.get(j)){
-                possibleedges.addAll(islands[j]);
-            }
-            Edge cheapestEdge = Collections.min(possibleedges); //Find the cheapest route from known islands
-            possibleedges.remove(cheapestEdge); //Removes it so it won't be checked in the future
-            if (!visitedIslands.get(cheapestEdge.dest)){ //If cheapest route endpoint hasnt been visited //TODO: Add thingy in case it has been visited
-                MSTedges.add(cheapestEdge); //Not sure if this is needed
-                possibleedges.addAll(islands[cheapestEdge.dest]); //Adds all routes from the destination of the cheapest edge
-                visitedIslands.set(cheapestEdge.dest); //Sets cheapest route as visited
+            Edge cheapestEdge = null;
+            cheapestEdge = queue.poll();
+            MSTedges.add(cheapestEdge);
+            if (!visitedIslands.get(cheapestEdge.src)){ //If cheapest route endpoint hasnt been visited //TODO: Add thingy in case it has been visited
+                queue.addAll(islands[cheapestEdge.src]); //Adds all routes from the destination of the cheapest edge
+                visitedIslands.set(cheapestEdge.src); //Sets cheapest route as visited
                 prices.add(cheapestEdge.weight); //Adds the price, for keeping tally
-
                 e++;
             }
-            if (j < numberOfIslands - 1){
+            if (!visitedIslands.get(cheapestEdge.dest)){ //If cheapest route endpoint hasnt been visited //TODO: Add thingy in case it has been visited
+                queue.addAll(islands[cheapestEdge.dest]); //Adds all routes from the destination of the cheapest edge
+                visitedIslands.set(cheapestEdge.dest); //Sets cheapest route as visited
+                prices.add(cheapestEdge.weight); //Adds the price, for keeping tally
+                e++;
+            }
+            if (j < numberOfIslands){
                 j++;
             }
         }
-
-
         Collections.sort(prices);
-
         int totalPris = 0;
         for(int i = 0; i < numberOfIslands-numberOfFerries-1;i++){
             totalPris += prices.get(i);
         }
         System.out.println(totalPris);
-
-        /*
-if (maxBridges > 6 && j == islands.length){
-                    infiniteloop();
-            }
-         */
-
-
-
     }
-
-    public static void infiniteloop(){
-        while (true){
-            System.out.println("Banana");
-        }
-    }
-
-    public static boolean isGood(int i, int j){
-        if (!visited[i] && !visited[j]) return false;
-        return !visited[i] || !visited[j]; //intellij wants to do magic
-    }
-
-/*
-    public static void prim(int[][] matrix){
-        visited = new boolean[numberOfIslands + 1];
-        visited[1] = true;
-        int goodEdges = 0;
-        while (goodEdges < numberOfIslands - 1){
-            int val1 = -1; int val2 = -1; int minPrice = Integer.MAX_VALUE; //Maybe make this higher
-            for (int i = 1; i <= numberOfIslands; i++){
-                for (int j = 1; j <= numberOfIslands; j++){
-                    int potPrice = matrix[j][i];
-                    if (potPrice < minPrice && potPrice != 0){
-                        if (isGood(j,i)){
-                            minPrice = potPrice;
-                            val1 = j;
-                            val2 = i;
-                            if (minPrice == 1) break;
-                        }
-                    }
-                }
-            }
-            if (val1 > 0 && val2 > 0){
-                goodEdges++;
-                priser[goodEdges-1] = minPrice;
-                visited[val1] = true;
-                visited[val2] = true;
-            }
-        }
-    }
-
-    public static void prim2(int[][] matrix){
-
-    }
-
- */
-
 }
 
 class Edge implements Comparable<Edge> {
@@ -132,6 +80,8 @@ class Edge implements Comparable<Edge> {
     }
     @Override
     public int compareTo(Edge o) {
+        if (this == null) return Integer.MAX_VALUE;
+        if (o == null) return Integer.MIN_VALUE;
         return this.weight - o.weight;
     }
 }
